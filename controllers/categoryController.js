eedbadf6ca22b8cd396ab14e7c1e5c9fbdc3d407
Cat-Tuong Tu <cattuongtu3@gmail.com>
2020-12-17 15:42:29 -0800

    Implemented GET for category delete page.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,7 +4,7 @@ let {
   body, 
   validationResult
 } = require("express-validator");
-const item = require("../models/item");
+const Item = require("../models/item");
 
 // Display list of all Categories
 exports.category_list = function (req, res) {
@@ -36,7 +36,16 @@ exports.category_detail = function (req, res, next) {
 }
 // Display category create form on GET.
 exports.category_create_get = function (req, res, next) {
-  // NOT YET IMPLEMENTED
+  // Display create form.
+  Category.find()
+    .exec(function (err, category) {
+      if (err) {
+        return next(err);
+      }
+      res.render("category_form.pug", {
+        category: category,
+      }
+    )});
 }
 
 // Handle category create on POST.
@@ -46,8 +55,28 @@ exports.category_create_post = [
 
 // Display Category delete form on GET.
 exports.category_delete_get = function (req, res, next) {
-  // NOT YET IMPLEMENTED
-}
+  // Display create form.
+  Category.findById(req.params.id)
+    .populate("category")
+    .populate("item")
+    .exec(function (err, cat) {
+      if (err) {
+        return next(err);
+      }
+      // If category is available, find all items
+      Item.find({category: cat})
+        .exec(function(err, items) {
+          if (err) {
+            return next(err);
+          }
+          // If item is available, render
+          res.render("category_delete.pug", {
+            category: cat,
+            category_item_list: items,
+          });
+      });
+    });
+  }
 
 // Handle Category delete on POST.
 exports.category_delete_post = function(req, res, next) {
