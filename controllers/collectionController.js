717c6dc5d571faad17eb7bd121a467c555cd0193
Cat-Tuong Tu <cattuongtu3@gmail.com>
2020-12-19 19:09:08 -0800

    Implemented update collection form on POST.

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -113,7 +113,7 @@ exports.collection_create_post = [
 					if (err) {
 						return next(err);
           }
-
+          
 					res.render("collection_form.pug", {
 						title: "Create Collection",
 						items: results.items,
@@ -200,15 +200,94 @@ exports.collection_update_get = function (req, res, next) {
 
 // Handle Collection update on POST
 exports.collection_update_post = [
-  (req, res, next) => {
-    // NOT YET IMPLEMENTED
-    next();
-  },
+  // Convert the items to an array
+	(req, res, next) => {
+		if (!(req.body.items instanceof Array)) {
+			if (typeof req.body.items === "undefined")
+				req.body.items = [];
+			else req.body.items = new Array(req.body.items);
+		}
+		next();
+	},
 
-  // Validate and sanitize fields.
+	// Validate and sanitise fields.
+	body("name", "Name must not be empty.")
+		.isLength({ min: 1 })
+		.trim(),
+	body("description", "Description must not be empty.")
+		.isLength({ min: 1 })
+    .trim(),
+  body("isLimited"),
 
-  // Process request after validation and sanitization.
-  (req, res, next) => {
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
 
-  }
+		// Create a ClothingCollection object with escaped/trimmed data and old id.
+		var collection = new ClothingCollection({
+			name: req.body.name,
+			description: req.body.description,
+			isLimited: req.body.isLimited == 'on' ? true : false,
+			items:
+				typeof req.body.items === "undefined"
+					? []
+					: req.body.items,
+			_id: req.params.id, //This is required, or a new ID will be assigned!
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all items for form.
+			async.parallel(
+				{
+					items: function (callback) {
+						Item.find({'_id' : {$in: collection.items}})
+              .populate('item')
+              .populate('category')
+              .exec(callback);
+					},
+          collection: 
+            ClothingCollection.findById(req.params.id)
+              .exec(callback)
+				},
+				function (err, results) {
+					if (err) {
+						return next(err);
+					}
+
+					// Mark our selected items as checked.
+					for (let i = 0; i < results.items.length; i++) {
+						if (
+							collection.items.indexOf(results.items[i]._id) > -1
+						) {
+							results.items[i].checked = "true";
+						}
+					}
+					res.render("collection_form.pug", {
+						title: "Update Collection",
+            collection: results.collection,
+            items: results.items,
+						errors: errors.array(),
+					});
+				}
+			);
+			return;
+		} else {
+			// Data from form is valid. Update the record.
+			ClothingCollection.findByIdAndUpdate(
+				req.params.id,
+				collection,
+				{},
+				function (err, collection) {
+					if (err) {
+						return next(err);
+					}
+					// Successful - redirect to collection detail page.
+					res.redirect(collection.url);
+				}
+			);
+		}
+	},
 ];
